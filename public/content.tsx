4f058bb9406126a3d3d9c5e6b4e32a6a36f0e7f0
willian-leal <willian.oliveira@bandtec.com.br>
2023-08-10 23:17:53 -0300

    ajustando página de formulário

--- public/content.tsx
+++ public/content.tsx
@@ -1,76 +1,218 @@
-import Link from 'next/link'
+import Link from "next/link"
 
 const content = {
   team: [
     {
-      name: 'Anna Guercio',
-      age: '21',
-      imageUrl: '/image/anna.png',
-      linkedinUrl: 'https://www.linkedin.com/in/annaguercio/',
-      githubUrl: 'https://github.com/anna-guercio'
+      name: "Anna Guercio",
+      age: "21",
+      imageUrl: "/image/anna.png",
+      linkedinUrl: "https://www.linkedin.com/in/annaguercio/",
+      githubUrl: "https://github.com/anna-guercio"
     },
     {
-      name: 'Kaique Lucena',
-      age: '21',
-      imageUrl: '/image/kaique.jpg',
-      linkedinUrl: 'https://www.linkedin.com/in/kaique-lucena-1383401a2/',
-      githubUrl: 'https://github.com/KaiqueeFigui'
+      name: "Kaique Lucena",
+      age: "21",
+      imageUrl: "/image/kaique.jpg",
+      linkedinUrl: "https://www.linkedin.com/in/kaique-lucena-1383401a2/",
+      githubUrl: "https://github.com/KaiqueeFigui"
     },
     {
-      name: 'Rodrigo Cunha',
-      age: '22',
-      imageUrl: '/image/rodrigo.jpeg',
+      name: "Rodrigo Cunha",
+      age: "22",
+      imageUrl: "/image/rodrigo.jpeg",
       linkedinUrl:
-        'https://www.linkedin.com/in/rodrigo-moreira-cunha-b92016177',
-      githubUrl: 'https://github.com/rodrigo-cunha-sptech'
+        "https://www.linkedin.com/in/rodrigo-moreira-cunha-b92016177",
+      githubUrl: "https://github.com/rodrigo-cunha-sptech"
     },
     {
-      name: 'Willian Leal',
-      age: '21',
-      imageUrl: '/image/will.png',
+      name: "Willian Leal",
+      age: "21",
+      imageUrl: "/image/will.png",
       linkedinUrl:
-        'https://www.linkedin.com/in/willian-leal-de-oliveira-0aab05192/',
-      githubUrl: 'https://github.com/willian-leal'
+        "https://www.linkedin.com/in/willian-leal-de-oliveira-0aab05192/",
+      githubUrl: "https://github.com/willian-leal"
     }
   ],
   homePage: {
-    title: 'Pesquisa de campo: extração de dados do YouTube',
+    title: "Pesquisa de campo: extração de dados do YouTube",
     subtitle: `Estamos precisando de ajuda para realizar nosso TCC! Nosso projeto envolve a criação de um modelo de Machine Learning capaz de analisar dados de recomendação. Para isso, precisamos coletar dados do histórico do YouTube de vários usuários para criar nossa base de dados.`,
-    logoUrl: '/image/logo.png'
+    logoUrl: "/image/logo.png"
   },
   learnMore: [
     {
-      title: 'Como vai funcionar a Extração?',
+      title: "Como vai funcionar a Extração?",
       subtitle: `A extração será realizada por meio do Google Takeout, recurso próprio do Google que permite exportar dados de vários serviços, incluindo o YouTube. Através dele, poderemos extrair os arquivos JSON do histórico de visualizações da conta, que serão carregados em nosso banco de dados.`
     },
     {
-      title: '“Mas você vai usar meus dados para realizar o projeto?”',
+      title: "“Mas você vai usar meus dados para realizar o projeto?”",
       subtitle: `Coletaremos apenas dados históricos de exibição do YouTube, que serão enviados anonimamente e tratados com base em dados. Pode ficar tranquilo!`,
       sx: {
-        textAlign: 'right'
+        textAlign: "right"
       }
     },
     {
-      title: '“Não entendo nada de computadores, não poderei ajudar.”',
+      title: "“Não entendo nada de computadores, não poderei ajudar.”",
       subtitle: `Não se preocupe! Não é necessário ter conhecimento de informática para realizar a extração. Montamos um passo a passo explicando tudo o que tem que ser feito.`
     }
   ],
   aboutUs: {
     title:
-      'Somos alunos de Ciência da Computação na faculdade SPTech e precisamos da sua ajuda para realizar nosso TCC',
+      "Somos alunos de Ciência da Computação na faculdade SPTech e precisamos da sua ajuda para realizar nosso TCC",
     subtitle:
-      'Nosso projeto envolve a elaboração de um modelo de Machine Learning de recomendação personalizada de vídeos do YouTube para usuários, com base em seus interesses e preferências, analisando seu comportamento de visualização e comparando-o com suas escolhas. Para desenvolver nosso modelo e criar um banco de dados relevante, precisamos coletar dados de usuários reais para tornar a IA mais realista e fornecer métricas precisas para o nosso projeto.'
+      "Nosso projeto envolve a elaboração de um modelo de Machine Learning de recomendação personalizada de vídeos do YouTube para usuários, com base em seus interesses e preferências, analisando seu comportamento de visualização e comparando-o com suas escolhas. Para desenvolver nosso modelo e criar um banco de dados relevante, precisamos coletar dados de usuários reais para tornar a IA mais realista e fornecer métricas precisas para o nosso projeto."
   },
   steps: [
     {
-      title: 'Passo 1 de 9',
+      title: "Passo 1 de 9",
       subtitle: (
         <>
-          Acesse o site do{' '}
+          Acesse o site do{" "}
           <Link href="https://takeout.google.com/">Google Takeout</Link>.
         </>
       )
     }
-  ]
+  ],
+  form: {
+    interestsList: [
+      "Ação",
+      "Animação",
+      "Artes e cultura",
+      "Ciência e tecnologia",
+      "Comédia",
+      "Culinária",
+      "Documentários",
+      "Drama",
+      "Educação e aprendizagem",
+      "Entretenimento em família",
+      "Esportes",
+      "Ficção científica",
+      "Filosofia e pensamento crítico",
+      "História e civilizações",
+      "Jogos eletrônicos",
+      "Meditação e yoga",
+      "Moda e beleza",
+      "Música",
+      "Política e assuntos sociais",
+      "Programas de entrevistas e talk shows",
+      "Programas de TV",
+      "Reality shows",
+      "Revisões de produtos e tecnologia",
+      "Saúde e bem-estar",
+      "Suspense",
+      "Terror",
+      "Tutoriais e guias de como fazer algo.",
+      "Viagens",
+      "Vídeos engraçados e memes",
+      "Vlogs"
+    ],
+    professionsList: [
+      "Administrador",
+      "Advogado",
+      "Agente de viagens",
+      "Analista de sistemas",
+      "Arquiteto",
+      "Artista plástico",
+      "Assessor de imprensa",
+      "Assistente administrativo",
+      "Assistente de marketing",
+      "Assistente social",
+      "Atendente de telemarketing",
+      "Ator",
+      "Auditor",
+      "Bibliotecário",
+      "Biólogo",
+      "Bombeiro",
+      "Cabeleireiro",
+      "Camareira",
+      "Cenógrafo",
+      "Chef de cozinha",
+      "Cientista da computação",
+      "Comissário de bordo",
+      "Comprador",
+      "Consultor de vendas",
+      "Contador",
+      "Corretor de imóveis",
+      "Coveiro",
+      "Cozinheiro",
+      "Dentista",
+      "Desenvolvedor",
+      "Designer",
+      "Designer de interiores",
+      "Designer gráfico",
+      "Detetive particular",
+      "Diretor de arte",
+      "Economista",
+      "Educador físico",
+      "Eletricista",
+      "Empresário",
+      "Engenheiro agrônomo",
+      "Engenheiro civil",
+      "Engenheiro de produção",
+      "Engenheiro de software",
+      "Enfermeiro",
+      "Escritor",
+      "Estagiário",
+      "Estilista",
+      "Estudante",
+      "Farmacêutico",
+      "Fisioterapeuta",
+      "Fotógrafo",
+      "Geógrafo",
+      "Geólogo",
+      "Gerente de recursos humanos",
+      "Gerente financeiro",
+      "Ginecologista",
+      "Historiador",
+      "Jardineiro",
+      "Jornalista",
+      "Juiz",
+      "Mecânico",
+      "Médico",
+      "Meteorologista",
+      "Militar",
+      "Modelo",
+      "Motorista",
+      "Músico",
+      "Nutricionista",
+      "Odontólogo",
+      "Office boy",
+      "Padeiro",
+      "Pedagogo",
+      "Pedreiro",
+      "Piloto",
+      "Policial",
+      "Porteiro",
+      "Programador",
+      "Psicólogo",
+      "Publicitário",
+      "Químico",
+      "Radialista",
+      "Recepcionista",
+      "Redator",
+      "Relações públicas",
+      "Revisor de texto",
+      "Secretário",
+      "Segurança",
+      "Servente",
+      "Soldado",
+      "Técnico em informática",
+      "Tradutor",
+      "Vendedor",
+      "Veterinário",
+      "Vigilante",
+      "Web designer",
+      "Zelador"
+    ],
+    educationalLevel: [
+      "Fundamental incompleto",
+      "Fundamental completo",
+      "Médio incompleto",
+      "Médio completo",
+      "Superior incompleto",
+      "Superior completo",
+      "Pós-graduação"
+    ],
+    genderList: ["Masculino", "Feminino", "Não-binário", "Outros"]
+  }
 }
 export default content
---
+++ src/app/form/page.tsx
@@ -0,0 +1,292 @@
+"use client"
+import { useState } from "react"
+import content from "../../../public/content"
+import {
+  Autocomplete,
+  Checkbox,
+  TextField,
+  FormControl,
+  Grid,
+  InputLabel,
+  MenuItem,
+  Select,
+  Button
+} from "@mui/material"
+import { Typography } from "../../components/Typography"
+import CheckBoxIcon from "@mui/icons-material/CheckBox"
+import CheckBoxOutlineBlankIcon from "@mui/icons-material/CheckBoxOutlineBlank"
+
+export interface FormValues {
+  email: string
+  age: number
+  gender: string
+  education: string
+  interests: string[]
+  profession: string
+}
+
+export interface FormEntity {
+  email: string
+  age: number
+  gender: String | undefined
+  education: String | undefined
+  interests: string[]
+  profession: String | undefined
+}
+
+const Form = () => {
+  const [values, setValues] = useState<FormValues>({
+    email: "",
+    age: 0,
+    gender: "",
+    education: "",
+    interests: [],
+    profession: ""
+  })
+
+  const icon = <CheckBoxOutlineBlankIcon fontSize="small" />
+  const checkedIcon = <CheckBoxIcon fontSize="small" />
+
+  const [file, setFile] = useState<File | null>(null)
+
+  const handleChange = (event: any) => {
+    const { name, value } = event.target
+
+    if (name === "interests") {
+      setValues({ ...values, [name]: event.target.value.split(",") })
+    } else {
+      setValues({ ...values, [name]: value })
+    }
+  }
+
+  const handleInterestsChange = (
+    event: React.ChangeEvent<{}>,
+    newValue: string[] | null
+  ) => {
+    if (newValue !== null) {
+      setValues({ ...values, interests: newValue })
+    }
+  }
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    uploadFile(file)
+    const formEntity = formValuesToFormEntity(values)
+    console.log(formEntity)
+  }
+
+  const handleDrop = (e: React.DragEvent<HTMLInputElement>) => {
+    e.preventDefault()
+    const file = e.dataTransfer.files[0]
+    setFile(file)
+    console.log("File dropped:", file)
+  }
+
+  const handleDragOver = (e: React.DragEvent<HTMLInputElement>) => {
+    e.preventDefault()
+  }
+
+  const handleDragLeave = (e: React.DragEvent<HTMLInputElement>) => {
+    e.preventDefault()
+  }
+
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0]
+    if (file) {
+      setFile(file)
+      console.log("File selected:", file)
+    }
+  }
+
+  const uploadFile = (file: File | null) => {
+    if (file) {
+      // Verifique se um arquivo foi selecionado antes de ler
+      const reader = new FileReader()
+      reader.onload = (event) => {
+        const fileContent = event.target?.result?.toString()
+        // Agora você pode usar o fileContent para o que precisar
+        // uploadToS3(fileContent, file?.name);
+      }
+      reader.readAsText(file, "utf-8")
+    }
+  }
+
+  const formValuesToFormEntity = (formValues: FormValues): FormEntity => {
+    const genderValue = formValues.gender
+    const educationValue = formValues.education
+    const professionValue = formValues.profession
+
+    const genderId = content.form.genderList.indexOf(genderValue) + 1
+    const educationId =
+      content.form.educationalLevel.indexOf(educationValue) + 1
+    const professionId =
+      content.form.professionsList.indexOf(professionValue) + 1
+
+    return {
+      email: formValues.email,
+      age: formValues.age,
+      gender: genderValue,
+      education: educationValue,
+      interests: formValues.interests,
+      profession: professionValue
+    }
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <Grid container sx={{ justifyContent: "center" }}>
+        <Grid
+          item
+          mt={5}
+          xs={6}
+          sx={{
+            justifyContent: "center"
+          }}
+        >
+          <Typography variant="subtitle" mb={1}>
+            Formulário
+          </Typography>
+          <Typography variant="paragraph" mb={4}>
+            Preencha o formulário com seus dados e submeta o arquivo da extração
+          </Typography>
+          <TextField
+            type="email"
+            id="email"
+            name="email"
+            onChange={handleChange}
+            value={values.email}
+            label="E-mail"
+            sx={{ width: "100%", mb: 3 }}
+          />
+
+          <TextField
+            type="number"
+            id="age"
+            name="age"
+            onChange={handleChange}
+            value={values.age}
+            label="Idade"
+            sx={{ width: "49.5%", mb: 3, mr: 0.5 }}
+          />
+
+          <FormControl sx={{ width: "49.5%", mb: 3 }}>
+            <InputLabel id="input-gender">Sexo</InputLabel>
+            <Select
+              labelId="input-gender"
+              id="gender"
+              value={values.gender}
+              name="gender"
+              onChange={handleChange}
+            >
+              {content.form.genderList.map((gender) => (
+                <MenuItem key={gender} value={gender}>
+                  {gender}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+
+          <FormControl sx={{ width: "49.5%", mb: 3, mr: 0.5 }}>
+            <InputLabel id="input-education">Nível de escolaridade</InputLabel>
+            <Select
+              labelId="input-education"
+              id="education"
+              value={values.education}
+              name="education"
+              onChange={handleChange}
+            >
+              {content.form.educationalLevel.map((education) => (
+                <MenuItem key={education} value={education}>
+                  {education}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+
+          <FormControl sx={{ width: "49.5%" }}>
+            <InputLabel id="input-profession">Profissão</InputLabel>
+            <Select
+              labelId="input-profession"
+              id="profession"
+              value={values.profession}
+              name="profession"
+              onChange={handleChange}
+            >
+              {content.form.professionsList.map((profession) => (
+                <MenuItem key={profession} value={profession}>
+                  {profession}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+          <Autocomplete
+            multiple
+            id="interests-checkboxes"
+            options={content.form.interestsList}
+            disableCloseOnSelect
+            getOptionLabel={(option) => option}
+            onChange={(event, newValue) =>
+              handleInterestsChange(event, newValue)
+            }
+            renderOption={(props, option, { selected }) => (
+              <li key={option} {...props}>
+                <Checkbox
+                  icon={icon}
+                  checkedIcon={checkedIcon}
+                  style={{ marginRight: 8 }}
+                  checked={selected}
+                />
+                {option}
+              </li>
+            )}
+            style={{ width: "100%", marginBottom: 20 }}
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                label="Interesses"
+                placeholder="Selecione"
+              />
+            )}
+          />
+
+          <Typography variant="paragraph" mb={1}>
+            Arraste o arquivo{" "}
+            <strong>
+              <i>historico-de-visualizacao.json</i>
+            </strong>{" "}
+            para a área abaixo
+          </Typography>
+          <input
+            type="file"
+            id="file"
+            name="file"
+            accept="image/*" // aqui é possível definir o tipo de arquivo aceito pelo input
+            onDrop={handleDrop}
+            onDragOver={handleDragOver}
+            onDragLeave={handleDragLeave}
+            onChange={handleFileChange}
+            style={{
+              width: "100%",
+              height: "180px",
+              border: "2px dashed gray",
+              borderRadius: "10px",
+              padding: "20px",
+              marginBottom: "20px"
+            }}
+          />
+          {file && (
+            <div>
+              <p>Arquivo selecionado: {file.name}</p>
+              <p>Tamanho: {file.size} bytes</p>
+            </div>
+          )}
+          <Button variant="contained" type="submit">
+            Enviar
+          </Button>
+        </Grid>
+      </Grid>
+    </form>
+  )
+}
+
+export default Form
