9d7ed995e08c38350bfa6d9d2ba6bd3bbd7c7bfe
José Paulo <jose.silva@bandtec.com.br>
2023-08-24 01:16:08 -0300

    add: retornando status code da resposta

--- src/hooks/useForm.tsx
+++ src/hooks/useForm.tsx
@@ -2,19 +2,23 @@ import { Form } from "interfaces"
 import { useCallback, useState } from "react"
 import { FormService } from "services"
 
-export const useForm = () => {
+
+export const useForm = (): {httpStatusCode: number, create: any} => {
   const [tasks, setTasks] = useState<Form[]>([])
+  const [httpStatusCode, setHttpStatusCode] = useState(0);
 
   const create = useCallback(async (form: Form, file: any) => {
     const { status, data } = await FormService.create(form, file)
 
-    if (status != 200) throw new Error()
+    setHttpStatusCode(status);
 
     setTasks(data)
+
+    return status;
   }, [])
 
   return {
-    tasks,
+    httpStatusCode,
     create
   }
 }
